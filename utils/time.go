6d8c3b1eae7cde29eefb5dacc04ec5a1b5cb3927
Hui <zikifaker@gmail.com>
2026-01-29 16:41:13 +0800

    feat(utils): add ParseAndValidateTimeRange

---
+++ utils/time.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"errors"
+	"time"
+)
+
+var (
+	ErrInvalidTimeFormat = errors.New("invalid time format, expected RFC3339")
+	ErrInvalidDateRange  = errors.New("start time must be before end time")
+)
+
+// ParseAndValidateTimeRange 解析并验证时间范围
+func ParseAndValidateTimeRange(startStr, endStr, timezone string) (start, end time.Time, err error) {
+	loc, err := time.LoadLocation(timezone)
+	if err != nil {
+		return time.Time{}, time.Time{}, err
+	}
+
+	start, err = time.ParseInLocation(time.RFC3339, startStr, loc)
+	if err != nil {
+		return time.Time{}, time.Time{}, ErrInvalidTimeFormat
+	}
+
+	end, err = time.ParseInLocation(time.RFC3339, endStr, loc)
+	if err != nil {
+		return time.Time{}, time.Time{}, ErrInvalidTimeFormat
+	}
+
+	if start.After(end) {
+		return time.Time{}, time.Time{}, ErrInvalidDateRange
+	}
+
+	return start, end, nil
+}
